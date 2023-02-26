622734f218d06d74fba1195d3bc3e2de7b285942
EnhengDui <94824676+EnhengDui@users.noreply.github.com>
2023-02-26 14:28:16 +0800

    pull test

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 //        System.out.println("Hello git!");
         System.out.println("Hello GitHub2!");
         System.out.println("Hello master test!");
-
+        System.out.println("Hello pull test!");
         System.out.println("Hello hot-fix test!");
     }
 }
